ce3775a748f32de67eac1663e4f24d7c4869c9dd
badugucs <badugucs@maveric-systems.com>
2022-11-08 17:04:58 +0530

    Initial code

---
+++ src/main/java/com/maveric/balanceservice/BalanceServiceApplication.java
@@ -0,0 +1,14 @@
+package com.maveric.balanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@SpringBootApplication
+public class BalanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceServiceApplication.class, args);
+	}
+
+}
